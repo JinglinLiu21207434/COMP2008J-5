02b012a7e4ffcbc25ad2232d8adfd4b40ae5eb39
Tao Puying <poying.tao@ucdconnect.ie>
2023-05-11 16:39:15 +0800

    picture

--- GamePanel.java
+++ GamePanel.java
@@ -1,5 +1,10 @@
 public class GamePanel {
     public static void main(String[] args) {
         System.out.println("我是大傻叉");
+        sb();
+    }
+    public static void sb(){
+        System.out.println("sb");
+        sb();
     }
 }
