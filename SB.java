79b3c96f084130c250adb79b68a55cc7f420b1a5
Tao Puying <poying.tao@ucdconnect.ie>
2023-05-11 16:40:00 +0800

    picture

--- SB.java
+++ SB.java
@@ -1,10 +1,9 @@
-public class GamePanel {
+public class SB {
     public static void main(String[] args) {
-        System.out.println("我是大傻叉");
         sb();
     }
     public static void sb(){
-        System.out.println("sb");
+        System.out.println("我是大傻逼");
         sb();
     }
 }
